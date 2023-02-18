89322a56796e6047fb5d9d558aabc3f08946b2b6
MadhavAgarwal01 <madhavagarwal505@gmail.com>
2023-02-18 23:28:01 +0530

    Initial setup done, maybe

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import dotenv from "dotenv"
 import express from "express";
 import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
 
 dotenv.config();
 const app = express();
@@ -21,6 +25,12 @@ mongoose.connection.on("disconnected", () => {
 app.get("/", (req, res) => {
     res.send("Hello bookster");
 });
+
+// Middlewares
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
 
 app.listen(8800, () => {
     connect();
