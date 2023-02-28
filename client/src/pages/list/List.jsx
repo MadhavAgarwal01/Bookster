ff2c7efbc02c050a7101057bba2a75b0d59e7140
MadhavAgarwal01 <madhavagarwal505@gmail.com>
2023-03-01 00:10:12 +0530

    List page made responsive

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -1,6 +1,10 @@
+import "./List.css";
+import "./List_res.css";
 import react, { useContext, useState } from "react";
 import React from 'react';
 import Select from 'react-select'
+import { useMediaQuery } from 'react-responsive';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useLocation } from "react-router-dom";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
@@ -8,10 +12,10 @@ import { DateRange } from "react-date-range";
 import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { format } from "date-fns"
-import "./List.css";
 import SearchItem from "../../components/searchItem/SearchItem";
 import useFetch from "../../hooks/useFetch";
 import { SearchContext } from "../../context/SearchContext";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function List() {
 
@@ -56,6 +60,8 @@ function List() {
         dispatch({ type: "NEW_SEARCH", payload: { destination, date, type, options } });
         reFetch();
     }
+    const isMobile = useMediaQuery({ query: `(max-width: 767px)` });
+    const [openSearchFilters, setOpenSearchFilters] = useState(false);
 
     return (
         <div>
@@ -63,73 +69,81 @@ function List() {
             <Header type="list" />
             <div className="listContainer">
                 <div className="listWrapper">
-                    <div className="listSearch">
-                        <h1 className="lsTitle">Search</h1>
-                        <div className="lsItem">
-                            <label>Destination</label>
-                            <input placeholder={destination} onChange={e => setDestination(e.target.value)} type="text" />
+                    {isMobile &&
+                        <div className="searchFilterToggle" onClick={() => setOpenSearchFilters(!openSearchFilters)}>
+                            <h3>Filters</h3>
+                            <FontAwesomeIcon className="hamburger" icon={faBars} />
                         </div>
-                        <div className="lsItem">
-                            <label>Check-in - Check-out</label>
-                            <span onClick={() => { setOpenDate(!openDate) }}>
-                                {`${format(date[0].startDate, "dd/MM/yyy")} to ${format(date[0].endDate, "dd/MM/yyy")}`}
-                            </span>
-                            {openDate &&
-                                <DateRange
-                                    editableDateInputs={true}
-                                    onChange={(item) => setDate([item.selection])}
-                                    moveRangeOnFirstSelection={false}
-                                    ranges={date}
-                                    minDate={new Date()}
-                                />}
-                        </div>
-                        <div className="lsItem">
-                            <label>Destination</label>
-                            <Select
-                                defaultValue={types[typeIndex]}
-                                isClearable={true}
-                                isSearchable={true}
-                                options={types}
-                                onChange={setSelectedOption}
-                            />
-                        </div>
-                        <div className="lsItem">
-                            <label>Options</label>
-                            <div className="lsOptions">
-                                <div className="lsOptionItem">
-                                    <span className="lsOptionText">
-                                        Min Price <small>per night</small>
-                                    </span>
-                                    <input type="number" onChange={e => setMin(e.target.value)} min={0} className="lsOptionInput" />
-                                </div>
-                                <div className="lsOptionItem">
-                                    <span className="lsOptionText">
-                                        Max Price <small>per night</small>
-                                    </span>
-                                    <input type="number" onChange={e => setMax(e.target.value)} min={0} className="lsOptionInput" />
-                                </div>
-                                <div className="lsOptionItem">
-                                    <span className="lsOptionText">
-                                        Adult
-                                    </span>
-                                    <input type="number" onChange={e => setOptions({ ...options, adult: e.target.value })} min={1} className="lsOptionInput" placeholder={options.adult} />
-                                </div>
-                                <div className="lsOptionItem">
-                                    <span className="lsOptionText">
-                                        Children
-                                    </span>
-                                    <input type="number" onChange={e => setOptions({ ...options, children: e.target.value })} min={0} className="lsOptionInput" placeholder={options.children} />
-                                </div>
-                                <div className="lsOptionItem">
-                                    <span className="lsOptionText">
-                                        Room
-                                    </span>
-                                    <input type="number" onChange={e => setOptions({ ...options, room: e.target.value })} min={1} className="lsOptionInput" placeholder={options.room} />
+                    }
+                    {(!isMobile || openSearchFilters) &&
+                        <div className="listSearch">
+                            <h1 className="lsTitle">Search</h1>
+                            <div className="lsItem">
+                                <label>Destination</label>
+                                <input placeholder={destination} onChange={e => setDestination(e.target.value)} type="text" />
+                            </div>
+                            <div className="lsItem">
+                                <label>Check-in - Check-out</label>
+                                <span onClick={() => { setOpenDate(!openDate) }}>
+                                    {`${format(date[0].startDate, "dd/MM/yyy")} to ${format(date[0].endDate, "dd/MM/yyy")}`}
+                                </span>
+                                {openDate &&
+                                    <DateRange
+                                        editableDateInputs={true}
+                                        onChange={(item) => setDate([item.selection])}
+                                        moveRangeOnFirstSelection={false}
+                                        ranges={date}
+                                        minDate={new Date()}
+                                    />}
+                            </div>
+                            <div className="lsItem">
+                                <label>Destination</label>
+                                <Select
+                                    defaultValue={types[typeIndex]}
+                                    isClearable={true}
+                                    isSearchable={true}
+                                    options={types}
+                                    onChange={setSelectedOption}
+                                />
+                            </div>
+                            <div className="lsItem">
+                                <label>Options</label>
+                                <div className="lsOptions">
+                                    <div className="lsOptionItem">
+                                        <span className="lsOptionText">
+                                            Min Price <small>per night</small>
+                                        </span>
+                                        <input type="number" onChange={e => setMin(e.target.value)} min={0} className="lsOptionInput" />
+                                    </div>
+                                    <div className="lsOptionItem">
+                                        <span className="lsOptionText">
+                                            Max Price <small>per night</small>
+                                        </span>
+                                        <input type="number" onChange={e => setMax(e.target.value)} min={0} className="lsOptionInput" />
+                                    </div>
+                                    <div className="lsOptionItem">
+                                        <span className="lsOptionText">
+                                            Adult
+                                        </span>
+                                        <input type="number" onChange={e => setOptions({ ...options, adult: e.target.value })} min={1} className="lsOptionInput" placeholder={options.adult} />
+                                    </div>
+                                    <div className="lsOptionItem">
+                                        <span className="lsOptionText">
+                                            Children
+                                        </span>
+                                        <input type="number" onChange={e => setOptions({ ...options, children: e.target.value })} min={0} className="lsOptionInput" placeholder={options.children} />
+                                    </div>
+                                    <div className="lsOptionItem">
+                                        <span className="lsOptionText">
+                                            Room
+                                        </span>
+                                        <input type="number" onChange={e => setOptions({ ...options, room: e.target.value })} min={1} className="lsOptionInput" placeholder={options.room} />
+                                    </div>
                                 </div>
                             </div>
+                            <button className="lsSearch" onClick={handleClick}>Search</button>
                         </div>
-                        <button className="lsSearch" onClick={handleClick}>Search</button>
-                    </div>
+                    }
                     <div className="listResult">
                         {loading ?
                             "loading"
